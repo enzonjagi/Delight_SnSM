245607a9becda78d71fc8b80937ff427c9bdfa31
enzonjagi <amosndungo@gmail.com>
2016-02-23 15:40:38 +0300

    Update[1]

--- hello.py
+++ hello.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 app = Flask(__name__)
+app.config('SECRET_KEY') = '1THrauphe.'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+    	                   current_time=datetime.utcnow())
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html')
@@ -20,6 +29,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
+class Nameform(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     app.run(debug=True)
